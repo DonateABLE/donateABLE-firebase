d544746a2fe8c6054d4c4987eeefd1b2fa2b4489
Adam Bibby <adam_bibby@hotmail.com>
2020-03-04 11:38:58 -0500

    comment

--- src/image-select.tsx
+++ src/image-select.tsx
@@ -3,6 +3,7 @@ import { Modal, ModalBody, ModalHeader, openModal } from 'components/modal'
 import { createElement, FunctionComponent, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import ReactCrop, { Crop, PercentCrop } from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
+import { bindArgs } from 'utils'
 
 async function getFile(): Promise<File | undefined> {
     return new Promise((resolve, reject) => {
@@ -29,47 +30,65 @@ const ImageEditor: FunctionComponent<{ file: File, onChange: (crop: Crop) => voi
     return <ReactCrop src={src} crop={crop} onChange={onChange} />
 }
 
+/**
+ * imageSelect opens a file dialog and prompts the user to crop the image
+ */
 export async function imageSelect(): Promise<File | undefined> {
+    // open the file dialog
     const f = await getFile()
     if (f === undefined) {
         return undefined
     }
 
-    switch (f.type) {
-        case 'image/png':
-            break
-        default:
-            throw new Error(`unsupported file type ${f.type}`)
+    // make sure the file has an image mime type
+    if (!f.type.match(/^image\/(png|jpe?g|gif|webp)/)) {
+        throw new Error(`unsupported file type ${f.type}`)
     }
 
     let crop: Crop = {}
     const cropChange = (c: Crop) => crop = c
 
-    await openModal(ctl => <Modal onCloseClick={ctl.close}>
+    // Open a modal with the image cropper in it
+    const saved = await openModal(ctl => <Modal onCloseClick={ctl.close}>
         <ModalHeader>
             Edit Image
         </ModalHeader>
         <ModalBody>
             <ImageEditor file={f} onChange={cropChange} />
-            <Button onClick={ctl.close}>Save</Button>
+            <Button onClick={bindArgs(true, ctl.resolve)}>Save</Button>
         </ModalBody>
-    </Modal>)
+    </Modal>, false)
+    // if the user didn't press save to close the model return undefined
+    if (!saved) {
+        return undefined
+    }
 
+    // resize the image and generate a new file based on the crop information
+
+    // I create a canvas at the size specified
     const size = 300
     const canvas = document.createElement('canvas')
     canvas.width = size
     canvas.height = size
     const ctx = canvas.getContext('2d')!
+
     const img = await image(URL.createObjectURL(f))
 
-    const { x = 0, y = 0, height = img.width, width = img.width } = crop
+    const {
+        x = 0,
+        y = 0,
+        height = img.height,
+        width = img.width,
+    } = crop
 
+    // calculate the scale and size of the new image
     const yScale = size / (img.height * (height / 100))
     const xScale = size / (img.width * (width / 100))
 
     const newWidth = img.width * xScale
     const newHeight = img.height * yScale
 
+    // draw the image to the canvas scaled and offset according to the crop
     ctx.drawImage(
         img,
         -(x / 100) * newWidth,
